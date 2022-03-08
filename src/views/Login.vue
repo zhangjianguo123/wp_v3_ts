709fd7cf1d0736cf5f0700bea8430be8e0e93356
zhangjianguo <2643876798@qq.com>
2022-03-08 11:25:57 +0800

    添加less

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>此文件不适用ts</div>
-    <div>多标签</div>
-    <div class="hr">
-        <p>画个三角形</p>
-    </div>
+  <div>此文件不适用ts</div>
+  <div>多标签</div>
+  <div class="hr">
+    <p>画个三角形{{b}}</p>
+  </div>
 </template>
-<script lang="ts" setup>
-console.log('打印文件不适用ts')
+<script setup>
+
+function newFn(x) {
+  console.log('newFn', x)
+  return function (y) {
+    return x + y
+  }
+}
+let b = newFn(3)
+console.log('b', b)
+let c = b(2)
+console.log('c', c, b(54))
 </script>
-<style lang="scss">
+<style lang="less">
 .hr {
-    background-color: pink;
+  background-color: pink;
+
+  p {
+    font-size: 20px;
+    font-weight: 700;
+  }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,12 @@ module.exports = {
     devServer: {
         open: true
     },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            }
-        ]
-    }
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.scss$/,
+    //             use: ["style-loader", "css-loader", "sass-loader"]
+    //         }
+    //     ]
+    // }
 }
