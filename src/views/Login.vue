2e5db0e877e7da2c04532b351ee1bd267940c12c
zhangjianguo <2643876798@qq.com>
2022-03-08 18:32:03 +0800

    函数柯里化实现

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,21 +2,44 @@
   <div>此文件不适用ts</div>
   <div>多标签</div>
   <div class="hr">
-    <p>画个三角形{{b}}</p>
+    <p>画个三角形</p>
   </div>
 </template>
 <script setup>
+let num = 0
+function curry(fn, currArgs) {
+  return function () {
+    let args = [].slice.call(arguments)
+    num++
+    console.log("这是调用时的参数", args,fn.length, num)
 
-function newFn(x) {
-  console.log('newFn', x)
-  return function (y) {
-    return x + y
+    // 首次调用时，若未提供最后一个参数currArgs，则不用进行args的拼接
+    if (currArgs !== undefined) {
+      num++
+      console.log("undefined", num, currArgs)
+      args = args.concat(currArgs);
+    }
+
+    // 递归调用
+    if (args.length < fn.length) {
+      num++
+      console.log("<<<<", num, args)
+      return curry(fn, args);
+    }
+    num++
+    console.log("递归出口",num)
+    // 递归出口
+    return fn.apply(null, args);
   }
 }
-let b = newFn(3)
-console.log('b', b)
-let c = b(2)
-console.log('c', c, b(54))
+
+function fn(a, b, c) {
+  console.log('打印this、', this)
+  return a + b + c
+}
+
+let newFun = curry(fn)
+console.log(newFun(1,)(2,56))
 </script>
 <style lang="less">
 .hr {
