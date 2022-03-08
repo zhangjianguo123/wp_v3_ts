aba729bb3f4ede0d4511c9be4c5da9958f651939
zhangjianguo <2643876798@qq.com>
2022-03-08 08:49:09 +0800

    初始化

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>此文件不适用ts</div>
+    <div>多标签</div>
+    <div class="hr">
+        <p>画个三角形</p>
+    </div>
+</template>
+<script lang="ts" setup>
+console.log('打印文件不适用ts')
+</script>
+<style lang="scss">
+.hr {
+    background-color: pink;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    devServer: {
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    }
+}
